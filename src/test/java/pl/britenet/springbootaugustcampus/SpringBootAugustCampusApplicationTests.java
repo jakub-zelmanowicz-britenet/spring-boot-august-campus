180cda1274410b6084bf863c942717cb0f116424
Jakub Zelmanowicz <jakub.zelmanowicz@britenet.com.pl>
2022-08-24 12:47:02 +0200

    Zajecia z 24.08

--- src/test/java/pl/britenet/springbootaugustcampus/SpringBootAugustCampusApplicationTests.java
+++ src/test/java/pl/britenet/springbootaugustcampus/SpringBootAugustCampusApplicationTests.java
@@ -1,13 +1,36 @@
 package pl.britenet.springbootaugustcampus;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.britenet.campus.database.DatabaseService;
+import pl.britenet.campus.model.Product;
+import pl.britenet.campus.service.ProductService;
+
+import java.util.Optional;
 
 @SpringBootTest
 class SpringBootAugustCampusApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testGetProductSuccessfully() {
+        DatabaseService databaseService = new DatabaseService();
+        ProductService productService = new ProductService(databaseService);
+
+        Optional<Product> oProduct = productService.getProduct(1);
+
+        Assertions.assertTrue(oProduct.isPresent());
+        Assertions.assertEquals("Motor", oProduct.get().getName());
+    }
+
+    @Test
+    void testGetProductNone() {
+        DatabaseService databaseService = new DatabaseService();
+        ProductService productService = new ProductService(databaseService);
+
+        Optional<Product> oProduct = productService.getProduct(100);
+
+        Assertions.assertFalse(oProduct.isPresent());
     }
 
 }
